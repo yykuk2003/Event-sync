27b51966661fa65f296d5670971bdaf23393a29a
js3692 <js3692@users.noreply.github.com>
2019-06-25 15:45:04 -0400

    Checkpoint

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import AddEvent from './AddEvent';
+// we import withTracker and Events into our app file
+import { withTracker } from 'meteor/react-meteor-data';
+import { Events } from "../api/events";
 
-class Todo extends Component {
+// Create a new React Component `EventApp`
+class EventApp extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <AddEvent />
+        {/* we have the pre tags to view contents of db just for verification */}
+        <pre>DB Stuff: {JSON.stringify(this.props, null, ' ')} </pre>
       </div>
     );
   }
 }
 
-export default Todo;
+// Wrap `EventApp` with the HOC withTracker and call the new component we get `App`
+const App = withTracker(() => {
+  return {
+    events: Events.find({}). fetch()
+  }
+})(EventApp);
+
+// export the component `App`
+export default App;
