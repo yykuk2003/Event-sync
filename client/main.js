e9805c9e9decefa566e02c77e2fd25379d603322
Jinho Yoon <yykuk2003@gmail.com>
2019-06-27 13:55:09 -0400

    blaze

--- client/main.js
+++ client/main.js
@@ -4,6 +4,34 @@ import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import App from '../imports/ui/App.js';
 
+import { Template } from 'meteor/templating';
+import './events.html';
+import AddEvent from './AddEvent.js';
+  import { Events } from '../api/events.js';
+  import ListEvents from './ListEvents.js';
+
+Template.events.helpers({
+  AddEvent() {
+    return AddEvent;
+  },
+
+  istEvents() {
+    return ListEvents
+  },
+
+  events() {
+    return Events.find({}).fetch();
+  },
+
+  onClick() {
+    const instance = Template.instance();
+    return () => {
+      instance.hasBeenClicked.set(true)
+    }
+  }
+})
+
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.getElementById('app'));
 });
+
